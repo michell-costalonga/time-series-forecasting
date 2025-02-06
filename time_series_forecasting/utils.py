17bcb02f4f5387d47b78b21394b51f44d6c9e995
Michell Costalonga <michell.costalonga@picpay.com>
2025-02-06 16:23:49 -0300

    Adição da função filter_dataframe

--- time_series_forecasting/utils.py
+++ time_series_forecasting/utils.py
@@ -1,3 +1,7 @@
+from pyspark.sql.functions import col, count, max
+from pyspark.sql.window import Window
+
+
 def rename_columns(df_pyspark_agg, target_column_name, date_column_name):
     """
     Retorna uma PySpark DataFrame com as colunas renomeadas adequadamente para realizar as previsões.
@@ -13,3 +17,30 @@ def rename_columns(df_pyspark_agg, target_column_name, date_column_name):
         date_column_name, "ds"
     ).withColumnRenamed(target_column_name, "y")
     return df_preprocessed
+
+
+def filter_dataframe(df_pyspark_agg):
+    """
+    Retorna dois PySpark DataFrames filtrados e preparados para o treino e previsão usando os algoritmos (modelo e média). Para o modelo, a contagem de valores deve ser maior que 80% do número máximo de dias preenchidos. Para os casos que não se encaixam na regra anterior, a média é usada para a previsão. As bandas superior e inferior são calculadas como o valor médio mais/menos duas vezes o desvio padrão.
+
+    :param df_pyspark_agg: PySpark DataFrame agregado por data e grupos de classificação com a contagem, ou valores que servirão para treinar os algoritmos.
+
+    :return: Dois PySpark DataFrames filtrados e prontos para o uso para a previsão.
+    """
+    columns_partition_by = [
+        col for col in df_pyspark_agg.columns if col not in ["ds", "y"]
+    ]
+
+    df_enriched = df_pyspark_agg.withColumn(
+        "filled_ds", count(col("ds")).over(Window.partitionBy(columns_partition_by))
+    ).withColumn(
+        "max_filled_ds", max(col("filled_ds")).over(Window.partitionBy(lit("all")))
+    )
+
+    df_filtered_model = df_enriched.filter(
+        col("filled_ds") > col("max_filled_ds") * 0.8
+    ).drop(*["filled_ds", "max_filled_ds"])
+    df_filtered_avg = df_enriched.filter(
+        col("filled_ds") <= col("max_filled_ds") * 0.8
+    ).drop(*["filled_ds", "max_filled_ds"])
+    return df_filtered_model, df_filtered_avg
